853147e610329e849be234de1523c2744fb1d074
Sumanthreddy-DE <sumanthreddy.de@gmail.com>
2023-11-12 16:47:22 +0100

    new setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your-package-name',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    author='Sumanthreddy',
+    author_email='sumanthreddy@example.com',
+    description='A short description of your package',
+    url='https://github.com/yourusername/your-package-name',
+)
